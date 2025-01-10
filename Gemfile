3b0d9b9dea391b7b15fb76796cd1b217695679d9
webdev219 <aleksaggcweb@gmail.com>
2025-01-10 17:44:31 +0100

    add uploader

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.2"
 
 gem "rails", "~> 7.0.3"
+gem "active_storage_validations", "0.9.8"
 gem "bcrypt"
 gem "faker"
 gem "sassc-rails", "2.1.2"
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -67,3 +67,16 @@ class MicropostSidebarTest < MicropostsInterface
   end
 end
 
+class ImageUploadTest < MicropostsInterface
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type=FILL_IN]'
+  end
+ 
+  test "should be able to attach an image" do
+    cont = "This micropost really ties the room together."
+    img = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: cont, image: img } }
+    assert FILL_IN.image.attached?
+  end
+end
